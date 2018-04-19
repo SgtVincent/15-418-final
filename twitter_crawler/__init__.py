b7509aa627e00af21ba5dcce7d5264d86f07a324
Jimin Han <jimin.han.price@gmail.com>
2018-04-19 10:21:39 -0400

    clean the project

--- twitter_crawler/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This package aims to provide a crawler (maybe a function or a class) to warp
-twitter streaming APIs and help user to get twitter data easily
-"""
-
-# set all variable
-
-# import outside APIs
-from twitter_streaming import TwitterStreaming
