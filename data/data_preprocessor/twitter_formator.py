4a67a8645a885f74468babefab2b5a31f0f218c7
Jimin Han <jimin.han.price@gmail.com>
2018-04-24 22:04:06 -0400

    add formator

--- data/data_preprocessor/twitter_formator.py
+++ data/data_preprocessor/twitter_formator.py
@@ -1,1 +1,19 @@
-# TODO: after filter, format a twitter into a prepared format
+import json
+
+
+def twitter_format(twitter):
+    twitter = json.loads(twitter)
+    formatted_twitter = {}
+    try:
+        formatted_twitter['Date'] = twitter['created_at']
+        formatted_twitter['text'] = twitter['text']
+        formatted_twitter['user_id'] = twitter['user']['id']
+        formatted_twitter['followers_count'] = twitter['user']['followers_count']
+        formatted_twitter['listed_count'] = twitter['user']['listed_count']
+        formatted_twitter['statuses_count'] = twitter['user']['statuses_count']
+        formatted_twitter['friends_count'] = twitter['user']['friends_count']
+        formatted_twitter['favourites_count'] = twitter['user']['favourites_count']
+    except:
+        pass
+    twitter = json.dumps(formatted_twitter)
+    return twitter
