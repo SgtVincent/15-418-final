d422b3bdecc1a8f10d2327a7fcd352fb39e0b1e4
Jimin Han <jimin.han.price@gmail.com>
2018-04-25 01:05:29 -0400

    fXXk cjt

--- data/data_preprocessor/tweets_store.py
+++ data/data_preprocessor/tweets_store.py
@@ -2,13 +2,10 @@ import time
 import sqlite3
 import json
 from os import path
+
 current_path = path.dirname(path.abspath(__file__))
 root_path = path.dirname(path.dirname(current_path))
 resource_path = path.join(root_path, "resources/")
-
-
-def ConvertTime(t_time):
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(t_time, '%a %b %d %H:%M:%S +0000 %Y'))
 
 
 def CreateTable(cursor):
@@ -23,20 +20,25 @@ def CreateTable(cursor):
         )''')
 
 
-def InsertTweet(cursor, row, ):
-    cursor.execute('INSERT INTO Tweets VALUES (?,?,?,?,?,?,?)', row)
+def InsertTweet(cursor, row):
+    b = cursor.execute('INSERT INTO Tweets VALUES (?,?,?,?,?,?,?)', row)
+    x = 1
 
 
 file_path = path.join(resource_path, "formatted_tweets.json")
 database_path = path.join(resource_path, "tweets.db")
-
 conn = sqlite3.connect(database_path)
 cursor = conn.cursor()
 CreateTable(cursor)
 
+count = 0
 with open(file_path, 'r') as f:
     for line in f:
+        count += 1
         js = json.loads(line)
         row = [js['Date'], js['text'], js['followers_count'], js['listed_count'], js['statuses_count'],
                js['friends_count'], js['favourites_count']]
         InsertTweet(cursor, row)
+
+for row in cursor.execute("SELECT * FROM Tweets"):
+    print(row)
