b42feae157478a6050faf01d34d1fd4468677ac9
SgtVincent <sgt79911030@gmail.com>
2018-05-06 00:02:24 -0400

    modification to script

--- scripts/run_env.sh
+++ scripts/run_env.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-cd ~
-git clone https://github.com/Han-xox/15-418-final.git
 sudo apt-get update
 sudo apt-get install python-pip python-dev -y
 pip install virtualenv
-cd ./15-418-final
 virtualenv --no-site-packages venv
 source ./venv/bin/activate
 pip install -r requirements.txt
